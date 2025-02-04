2fdecf7934005c0787aca3553020c6abf6b4cd2a
sermonfire <2900450395@qq.com>
2025-02-04 12:02:18 +0800

    main.js文件更新

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate';
 
 import App from './App.vue'
 import router from './router'
 
+import TDesign from 'tdesign-vue-next';
+import 'tdesign-vue-next/es/style/index.css';
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(createPersistedState()))
 app.use(router)
+app.use(TDesign)
 
 app.mount('#app')
